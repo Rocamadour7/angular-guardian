68f6fe4e53bb76d8045cd059a7fd3c05825638c1
Luis Molina <luis_molina@outlook.com>
2017-05-01 05:13:29 -0600

    Added placeholder when no hero/villain selected

--- src/app/heroes/hero-routing.module.ts
+++ src/app/heroes/hero-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HeroesComponent } from './heroes.component';
+import { HeroStartComponent } from './hero-start/hero-start.component';
 import { HeroesDetailsComponent } from './heroes-details/heroes-details.component';
 
 const heroesRoutes: Routes = [
     { path: 'heroes', component: HeroesComponent, children: [
+        { path: '', component: HeroStartComponent },
         { path: ':slug', component: HeroesDetailsComponent }
     ] }
 ];
--- src/app/villains/villain-routing.module.ts
+++ src/app/villains/villain-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { VillainsComponent } from './villains.component';
+import { VillainStartComponent } from './villain-start/villain-start.component';
 import { VillainsDetailsComponent } from './villains-details/villains-details.component';
 
 const villainsRoutes: Routes = [
     { path: 'villains', component: VillainsComponent, children: [
+        { path: '', component: VillainStartComponent },
         { path: ':slug', component: VillainsDetailsComponent }
     ] }
 ];
