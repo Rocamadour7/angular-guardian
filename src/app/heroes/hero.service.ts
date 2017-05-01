c84d76302aeff893dba392de90a58f37334d850a
Luis Molina <luis_molina@outlook.com>
2017-05-01 16:29:23 -0600

    Changed URLs to relative

--- src/app/heroes/hero.service.ts
+++ src/app/heroes/hero.service.ts
@@ -11,8 +11,8 @@ export class HeroService {
       name: 'Peter Quill',
       specie: 'Human/Celestial Hybrid',
       description: 'A half-human who was kidnapped from Earth by the Ravagers at the age of eight. After living in space for nearly thirty years, Quill became wrapped up in an intergalactic conspiracy that introduced him to the Guardians, with him becoming their leader. He is the pilot of the Milano.',
-      imgPathAvatar: '/assets/heroes/StarLord.png',
-      imgPath: '/assets/heroes/StarLord2.png',
+      imgPathAvatar: 'assets/heroes/StarLord.png',
+      imgPath: 'assets/heroes/StarLord2.png',
       slug: 'StarLord'
     },
     {
@@ -20,8 +20,8 @@ export class HeroService {
       name: 'Gamora',
       specie: 'Zehoberei (Cyborg)',
       description: 'The only known survivor of the Zehoberei race, adopted by the Mad Titan Thanos. Renowned as the galaxy\'s deadliest assassin, Gamora had a falling out with her father that caused her to rebel against him, thereby making her an enemy of Thanos and his associates. Her fugitive status led to her becoming a member of the Guardians. She is known as the "Deadliest Woman in the Galaxy".',
-      imgPathAvatar: '/assets/heroes/Gamora.png',
-      imgPath: '/assets/heroes/Gamora2.png',
+      imgPathAvatar: 'assets/heroes/Gamora.png',
+      imgPath: 'assets/heroes/Gamora2.png',
       slug: 'Gamora'
     },
     {
@@ -29,8 +29,8 @@ export class HeroService {
       name: 'Drax the Destroyer',
       specie: 'Human',
       description: 'An enigma, with his true origin unknown. He is driven only by a burning desire to obtain vengeance against Ronan of the Kree, who killed Drax\'s wife and daughter. Drax\'s search for revenge resulted in him joining the Guardians.',
-      imgPathAvatar: '/assets/heroes/Drax.png',
-      imgPath: '/assets/heroes/Drax2.png',
+      imgPathAvatar: 'assets/heroes/Drax.png',
+      imgPath: 'assets/heroes/Drax2.png',
       slug: 'Drax-the-Destroyer'
     },
     {
@@ -38,8 +38,8 @@ export class HeroService {
       name: 'Rocket Raccoon',
       specie: 'Raccoon',
       description: 'A creature resembling a Terran raccoon that was genetically modified by an alien race, giving him human level intelligence. However, years of repetitive genetic rewrite and abuse resulted in Rocket developing a cynical and vain personality, making him an ideal mercenary and teammate to the other Guardians.',
-      imgPathAvatar: '/assets/heroes/Rocket.png',
-      imgPath: '/assets/heroes/Rocket2.png',
+      imgPathAvatar: 'assets/heroes/Rocket.png',
+      imgPath: 'assets/heroes/Rocket2.png',
       slug: 'Rocket-Raccoon'
     },
     {
@@ -47,8 +47,8 @@ export class HeroService {
       name: 'Groot',
       specie: 'Flora colossus',
       description: 'A sentient tree-like individual and accomplice of Rocket Raccoon. Groot\'s physiology renders the limits of his speech to the phrase, "I am Groot.", and "We are Groot.", due to having a wooden larynx. Though Groot\'s vocabulary is only limited to five words, he is usually trying to communicate other meanings. The only way to understand Groot\'s speech is for someone to translate what he is trying to say, which up until now only Rocket is shown to be able to do so.',
-      imgPathAvatar: '/assets/heroes/Groot.png',
-      imgPath: '/assets/heroes/Groot2.png',
+      imgPathAvatar: 'assets/heroes/Groot.png',
+      imgPath: 'assets/heroes/Groot2.png',
       slug: 'Groot'
     },
     {
@@ -56,8 +56,8 @@ export class HeroService {
       name: 'Mantis',
       specie: 'Unknown',
       description: 'A young woman raised by Ego.',
-      imgPathAvatar: '/assets/heroes/Mantis.png',
-      imgPath: '/assets/heroes/Mantis2.png',
+      imgPathAvatar: 'assets/heroes/Mantis.png',
+      imgPath: 'assets/heroes/Mantis2.png',
       slug: 'Mantis'
     },
   ];
--- src/app/villains/villain.service.ts
+++ src/app/villains/villain.service.ts
@@ -11,8 +11,8 @@ export class VillainService {
       name: 'Thanos',
       specie: 'Titanian',
       description: 'Thanos, also known as the Dark Lord by his most loyal underlings and the Mad Titan by the galactic communities, is a powerful, cosmic warlord who rules over a distant region of space and also commands a massive army known as the Chitauri. His own main objective is to obtain the Infinity Stones, and his desire to achieve this goal is what led him to forging deals with the villains Loki and Ronan the Accuser in their respective campaigns against Earth and Xandar. Both of these new alliances cost Thanos much of his resources, including one of the stones and the loyalty of his two daughters, Gamora and Nebula, and also inadvertently resulted in the formation of the two superhero groups the Avengers and the Guardians of the Galaxy. Eventually, he lost his patience with his servants and opted to seek out the Stones personally.',
-      imgPathAvatar: '/assets/villains/Thanos.png',
-      imgPath: '/assets/villains/Thanos2.png',
+      imgPathAvatar: 'assets/villains/Thanos.png',
+      imgPath: 'assets/villains/Thanos2.png',
       slug: 'Thanos'
     },
     {
@@ -20,8 +20,8 @@ export class VillainService {
       name: 'Ayesha',
       specie: 'Sovereign',
       description: 'Ayesha is the Golden High Priestess of the Sovereign who is brought into conflict with the Guardians of the Galaxy.',
-      imgPathAvatar: '/assets/villains/Ayesha.png',
-      imgPath: '/assets/villains/Ayesha2.png',
+      imgPathAvatar: 'assets/villains/Ayesha.png',
+      imgPath: 'assets/villains/Ayesha2.png',
       slug: 'Ayesha'
     },
     {
@@ -29,8 +29,8 @@ export class VillainService {
       name: 'Sakaarans',
       specie: 'Sakaarans',
       description: 'The Sakaarans are a sentient insectoid race native to the planet Sakaar, which they share with a large number of stranded individuals of many different species. A client race of the Kree, Sakaaran soldiers were commonly placed within Kree military ranks.',
-      imgPathAvatar: '/assets/villains/Sakaaran.png',
-      imgPath: '/assets/villains/Sakaaran2.png',
+      imgPathAvatar: 'assets/villains/Sakaaran.png',
+      imgPath: 'assets/villains/Sakaaran2.png',
       slug: 'Sakaarans'
     },
     {
@@ -38,8 +38,8 @@ export class VillainService {
       name: 'Ronan the Accuser',
       specie: 'Kree',
       description: 'Ronan, also known as Ronan the Accuser was a radical member of the Kree race who was disgusted by a peace treaty made between the Kree Empire and Xandar. A ferocious, genocidal militarist, Ronan attempted to eradicate the Xandarians by forging an alliance with the Mad Titan Thanos, offering the Orb in exchange for the destruction of Xandar. After a long search, Ronan acquired the Orb but, having seen it\'s true destructive capabilities, decided against giving it to Thanos, choosing to take the Orb\'s power for himself. Using his new-found power, he attacked Xandar, but was defeated and killed by the combined strength of the Guardians of the Galaxy.',
-      imgPathAvatar: '/assets/villains/Ronan.png',
-      imgPath: '/assets/villains/Ronan2.png',
+      imgPathAvatar: 'assets/villains/Ronan.png',
+      imgPath: 'assets/villains/Ronan2.png',
       slug: 'Ronan-the-Accuser'
     },
     {
@@ -47,8 +47,8 @@ export class VillainService {
       name: 'Korath the Pursuer',
       specie: 'Kree',
       description: 'Korath the Pursuer was a Kree mercenary working for Ronan the Accuser under the order of Thanos with orders to locate the Orb. He choose to remained loyal to Ronan once the Orb was found and was killed by Drax the Destroyer during the Battle of Xandar.',
-      imgPathAvatar: '/assets/villains/Korath.png',
-      imgPath: '/assets/villains/Korath2.png',
+      imgPathAvatar: 'assets/villains/Korath.png',
+      imgPath: 'assets/villains/Korath2.png',
       slug: 'Korath-the-Pursuer'
     },
   ];
